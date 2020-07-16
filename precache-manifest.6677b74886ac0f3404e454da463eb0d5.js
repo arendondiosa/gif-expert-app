d0bf87df4ccdf94d41f84356eeb82a0b0be98a9a
Alejandro E. Rendon <alejandro@rendon.co>
2020-07-15 20:54:30 -0500

    Updates

---
+++ precache-manifest.6677b74886ac0f3404e454da463eb0d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51b4a72ce8d5c0b4c35e03af1f6bc4b",
+    "url": "/gif-expert-app/index.html"
+  },
+  {
+    "revision": "b50c4f6c9cbb5866a665",
+    "url": "/gif-expert-app/static/css/2.57019924.chunk.css"
+  },
+  {
+    "revision": "fb85a53b5988ad88a9ec",
+    "url": "/gif-expert-app/static/css/main.ad6a1c67.chunk.css"
+  },
+  {
+    "revision": "b50c4f6c9cbb5866a665",
+    "url": "/gif-expert-app/static/js/2.747775cf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gif-expert-app/static/js/2.747775cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb85a53b5988ad88a9ec",
+    "url": "/gif-expert-app/static/js/main.e3fb4c90.chunk.js"
+  },
+  {
+    "revision": "12a08dbff82fbedb8c21",
+    "url": "/gif-expert-app/static/js/runtime-main.dc959591.js"
+  }
+]);
